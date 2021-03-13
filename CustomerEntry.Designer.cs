f0d0b0988cbbf42645d0f7a707e2e5d04932d19e
Rohail <Rohail@ROHAIL-DESKTOP>
2021-03-12 19:41:43 -0500

    Secondary form almost fully complete (not coded).

--- CustomerEntry.Designer.cs
+++ CustomerEntry.Designer.cs
@@ -29,6 +29,7 @@ namespace CustomerViewer
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.comboBoxTitle = new System.Windows.Forms.ComboBox();
             this.checkBoxVip = new System.Windows.Forms.CheckBox();
             this.textBoxLastName = new System.Windows.Forms.TextBox();
@@ -36,6 +37,16 @@ namespace CustomerViewer
             this.labelLastName = new System.Windows.Forms.Label();
             this.labelFirstName = new System.Windows.Forms.Label();
             this.labelTitle = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.listViewEntries = new System.Windows.Forms.ListView();
+            this.columnHeaderVIP = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderTitle = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderFirstName = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderLastName = new System.Windows.Forms.ColumnHeader();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // comboBoxTitle
@@ -59,7 +70,8 @@ namespace CustomerViewer
             this.comboBoxTitle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxTitle.Name = "comboBoxTitle";
             this.comboBoxTitle.Size = new System.Drawing.Size(133, 23);
-            this.comboBoxTitle.TabIndex = 12;
+            this.comboBoxTitle.TabIndex = 1;
+            this.toolTip.SetToolTip(this.comboBoxTitle, "Select the customer\'s title.");
             // 
             // checkBoxVip
             // 
@@ -69,8 +81,9 @@ namespace CustomerViewer
             this.checkBoxVip.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkBoxVip.Name = "checkBoxVip";
             this.checkBoxVip.Size = new System.Drawing.Size(46, 19);
-            this.checkBoxVip.TabIndex = 17;
+            this.checkBoxVip.TabIndex = 6;
             this.checkBoxVip.Text = "&VIP:";
+            this.toolTip.SetToolTip(this.checkBoxVip, "Select if the customer is a VIP.");
             this.checkBoxVip.UseVisualStyleBackColor = true;
             // 
             // textBoxLastName
@@ -79,7 +92,8 @@ namespace CustomerViewer
             this.textBoxLastName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxLastName.Name = "textBoxLastName";
             this.textBoxLastName.Size = new System.Drawing.Size(132, 23);
-            this.textBoxLastName.TabIndex = 16;
+            this.textBoxLastName.TabIndex = 5;
+            this.toolTip.SetToolTip(this.textBoxLastName, "Enter the customer\'s last name.");
             // 
             // textBoxFirstName
             // 
@@ -87,14 +101,15 @@ namespace CustomerViewer
             this.textBoxFirstName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxFirstName.Name = "textBoxFirstName";
             this.textBoxFirstName.Size = new System.Drawing.Size(132, 23);
-            this.textBoxFirstName.TabIndex = 14;
+            this.textBoxFirstName.TabIndex = 3;
+            this.toolTip.SetToolTip(this.textBoxFirstName, "Enter the customer\'s first name.");
             // 
             // labelLastName
             // 
             this.labelLastName.Location = new System.Drawing.Point(58, 92);
             this.labelLastName.Name = "labelLastName";
             this.labelLastName.Size = new System.Drawing.Size(131, 15);
-            this.labelLastName.TabIndex = 15;
+            this.labelLastName.TabIndex = 4;
             this.labelLastName.Text = "&Last Name:";
             this.labelLastName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
@@ -103,7 +118,7 @@ namespace CustomerViewer
             this.labelFirstName.Location = new System.Drawing.Point(58, 63);
             this.labelFirstName.Name = "labelFirstName";
             this.labelFirstName.Size = new System.Drawing.Size(131, 15);
-            this.labelFirstName.TabIndex = 13;
+            this.labelFirstName.TabIndex = 2;
             this.labelFirstName.Text = "&First Name:";
             this.labelFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
@@ -112,15 +127,96 @@ namespace CustomerViewer
             this.labelTitle.Location = new System.Drawing.Point(58, 35);
             this.labelTitle.Name = "labelTitle";
             this.labelTitle.Size = new System.Drawing.Size(131, 15);
-            this.labelTitle.TabIndex = 11;
+            this.labelTitle.TabIndex = 0;
             this.labelTitle.Text = "&Title:";
             this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelError
+            // 
+            this.labelError.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelError.Location = new System.Drawing.Point(12, 434);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(360, 66);
+            this.labelError.TabIndex = 8;
+            this.toolTip.SetToolTip(this.labelError, "Displays all error messages.");
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(297, 513);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 11;
+            this.buttonExit.Text = "E&xit";
+            this.toolTip.SetToolTip(this.buttonExit, "Click to exit the application.");
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(161, 513);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 10;
+            this.buttonReset.Text = "&Reset";
+            this.toolTip.SetToolTip(this.buttonReset, "Click to reset all entry fields.");
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(12, 513);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnter.TabIndex = 9;
+            this.buttonEnter.Text = "&Enter";
+            this.toolTip.SetToolTip(this.buttonEnter, "Click to enter the current customer.");
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            // 
+            // listViewEntries
+            // 
+            this.listViewEntries.CheckBoxes = true;
+            this.listViewEntries.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderVIP,
+            this.columnHeaderTitle,
+            this.columnHeaderFirstName,
+            this.columnHeaderLastName});
+            this.listViewEntries.HideSelection = false;
+            this.listViewEntries.Location = new System.Drawing.Point(12, 153);
+            this.listViewEntries.Name = "listViewEntries";
+            this.listViewEntries.Size = new System.Drawing.Size(360, 266);
+            this.listViewEntries.TabIndex = 7;
+            this.toolTip.SetToolTip(this.listViewEntries, "Displays a list of all customers.");
+            this.listViewEntries.UseCompatibleStateImageBehavior = false;
+            this.listViewEntries.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderVIP
+            // 
+            this.columnHeaderVIP.Text = "VIP?";
+            this.columnHeaderVIP.Width = 30;
+            // 
+            // columnHeaderTitle
+            // 
+            this.columnHeaderTitle.Text = "Title";
+            this.columnHeaderTitle.Width = 45;
+            // 
+            // columnHeaderFirstName
+            // 
+            this.columnHeaderFirstName.Text = "First Name";
+            this.columnHeaderFirstName.Width = 120;
+            // 
+            // columnHeaderLastName
+            // 
+            this.columnHeaderLastName.Text = "Last Name";
+            this.columnHeaderLastName.Width = 120;
             // 
             // CustomerEntry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.listViewEntries);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.labelError);
             this.Controls.Add(this.comboBoxTitle);
             this.Controls.Add(this.checkBoxVip);
             this.Controls.Add(this.textBoxLastName);
@@ -144,5 +240,15 @@ namespace CustomerViewer
         private System.Windows.Forms.Label labelLastName;
         private System.Windows.Forms.Label labelFirstName;
         private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ListView listViewEntries;
+        private System.Windows.Forms.ColumnHeader columnHeaderVIP;
+        private System.Windows.Forms.ColumnHeader columnHeaderTitle;
+        private System.Windows.Forms.ColumnHeader columnHeaderFirstName;
+        private System.Windows.Forms.ColumnHeader columnHeaderLastName;
     }
 }
